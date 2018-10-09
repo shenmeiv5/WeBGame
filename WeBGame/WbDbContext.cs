0c7918cb3b51847bd83056a158801ddf21e39925
Egan <1154610080@qq.com>
2018-10-10 00:14:40 +0800

    配置默认端口，首页，服务层和仓储层的依赖注入和EF

---
+++ WeBGame/WbDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeBGame.Models;
+
+namespace WeBGame
+{
+    /// <summary>
+    /// 数据库上下文操作类
+    /// </summary>
+    public class WbDbContext:DbContext
+    {
+        public WbDbContext(DbContextOptions<WbDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //关联配置
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
