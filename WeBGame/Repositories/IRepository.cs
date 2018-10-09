fb42e272f6652292e8010cc0fb9515c24f7f3858
Egan <1154610080@qq.com>
2018-10-10 00:16:41 +0800

    创建仓储层，完成仓储基类和用户仓储类

---
+++ WeBGame/Repositories/IRepository.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using WeBGame.Models;
+
+namespace WeBGame.Repositories
+{
+    /// <summary>
+    /// 仓储接口
+    /// </summary>
+    public interface IRepository
+    {
+        
+    }
+
+    /// <summary>
+    /// 泛型仓储接口
+    /// </summary>
+    /// <typeparam name="TEntity">实体类型</typeparam>
+    /// <typeparam name="TPrimaryKey">主键类型</typeparam>
+    public interface IRepository<TEntity, TPrimaryKey> : IRepository where TEntity : Entity<TPrimaryKey>
+    {
+        /// <summary>
+        /// 获取实体集合
+        /// </summary>
+        /// <returns></returns>
+        List<TEntity> GetAllList();
+
+        /// <summary>
+        /// 根据 lambda 获取实体集合
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        List<TEntity> GetAllList(Expression<Func<TEntity, bool>> expression);
+
+        /// <summary>
+        /// 根据主键获取实体
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        TEntity GetEntity(TPrimaryKey id);
+
+        /// <summary>
+        /// 根据 lambda 获取单个实体
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        TEntity GetEntity(Expression<Func<TEntity, bool>> expression);
+
+        /// <summary>
+        /// 插入实体
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <returns></returns>
+        TEntity Insert(TEntity entity);
+
+        /// <summary>
+        /// 更新实体
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <returns></returns>
+        TEntity Update(TEntity entity);
+
+        /// <summary>
+        /// 插入或更新实体
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <returns></returns>
+        TEntity InsertOrUpdate(TEntity entity);
+
+        /// <summary>
+        /// 删除实体
+        /// </summary>
+        /// <param name="entity">待删除实体</param>
+        /// <returns></returns>
+        void DeleteEntity(TEntity entity);
+
+        /// <summary>
+        /// 删除实体
+        /// </summary>
+        /// <param name="id">待删除实体的主键</param>
+        /// <returns></returns>
+        void DeleteEntity(TPrimaryKey id);
+    }
+
+    /// <summary>
+    /// 默认Long类型主键仓储接口
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IRepository<TEntity> : IRepository<TEntity, long> where TEntity : Entity { }
+}
---
+++ WeBGame/Repositories/RepositoryBase.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using WeBGame;
+using WeBGame.Models;
+using WeBGame.Repositories;
+
+namespace WeBGame.Repositories
+{
+    /// <summary>
+    /// 仓储基类
+    /// </summary>
+    public abstract class RepositoryBase<TEntity, TPrimaryKey>:IRepository<TEntity, TPrimaryKey> where TEntity:Entity<TPrimaryKey>
+    {
+        /// <summary>
+        /// 数据库访问上下文
+        /// </summary>
+        protected readonly WbDbContext _dbContext;
+
+        protected RepositoryBase(WbDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public List<TEntity> GetAllList()
+        {
+            return _dbContext.Set<TEntity>().ToList();
+        }
+
+        public List<TEntity> GetAllList(Expression<Func<TEntity, bool>> expression)
+        {
+            return _dbContext.Set<TEntity>().Where(expression).ToList();
+        }
+
+        public TEntity GetEntity(TPrimaryKey id)
+        {
+            return _dbContext.Set<TEntity>().FirstOrDefault(CreateExpressionForId(id));
+        }
+
+        public TEntity GetEntity(Expression<Func<TEntity, bool>> expression)
+        {
+            return _dbContext.Set<TEntity>().Where(expression).FirstOrDefault();
+        }
+
+        public TEntity Insert(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Add(entity);
+            return entity;
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Attach(entity);
+            //标记该实体已被修改
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            return entity;
+        }
+
+        public TEntity InsertOrUpdate(TEntity entity)
+        {
+            if (GetEntity(entity.Id) != null)
+                return this.Update(entity);
+            return this.Insert(entity);
+        }
+
+        public void DeleteEntity(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+        }
+
+        public void DeleteEntity(TPrimaryKey id)
+        {
+            _dbContext.Set<TEntity>().Remove(GetEntity(id));
+        }
+
+        /// <summary>
+        /// 保存事务
+        /// </summary>
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// 根据主键创建 lambda 表达式
+        /// </summary>
+        /// <param name="id">主键</param>
+        /// <returns></returns>
+        protected static Expression<Func<TEntity, bool>> CreateExpressionForId(TPrimaryKey id)
+        {
+            var lambdaParm = Expression.Parameter(typeof(TEntity));
+            var lambdaBody = Expression.Equal(
+                Expression.PropertyOrField(lambdaParm, "Id"),
+                Expression.Constant(id, typeof(TPrimaryKey))
+            );
+
+            return Expression.Lambda<Func<TEntity, bool>>(lambdaBody, lambdaParm);
+        }
+    }
+
+}
+
+public abstract class RepositoryBase<TEntity> : RepositoryBase<TEntity, long> where TEntity : Entity
+{
+    protected RepositoryBase(WbDbContext dbContext) : base(dbContext)
+    {
+    }
+}
