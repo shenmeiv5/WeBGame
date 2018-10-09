6ba1036b60c7a199c3332194d9d37216c881fcda
Egan <1154610080@qq.com>
2018-10-10 00:15:59 +0800

    创建实体基类和用户实体

---
+++ WeBGame/Models/Entity.cs
@@ -0,0 +1,22 @@
+﻿namespace WeBGame.Models
+{
+    /// <summary>
+    /// 泛型实体基类
+    /// </summary>
+    /// <typeparam name="TPrimeKey">主键类型</typeparam>
+    public abstract class Entity<TPrimeKey>
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public virtual TPrimeKey Id { get; set; }
+    }
+
+    /// <summary>
+    /// 默认主键类型为Long的实体基类
+    /// </summary>
+    public abstract class Entity:Entity<long>
+    {
+
+    }
+}
