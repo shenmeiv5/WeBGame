bffb53f35084c920588ff607ce81017dfb3b8179
Egan <1154610080@qq.com>
2018-10-10 00:16:58 +0800

    创建服务层，完成用户服务类

---
+++ WeBGame/Service/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using WeBGame.Models;
+
+namespace WeBGame.Service
+{
+    /// <summary>
+    /// 用户服务接口
+    /// </summary>
+    public interface IUserService
+    {
+        /// <summary>
+        /// 获取用户
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        User GetUser(string username, string password);
+    }
+}
