1af92b6413b467172b123f912abffaeb48596344
AshaSalorina <he670004272@gmail.com>
2018-10-09 20:11:38 +0900

    尝试加入线程动态更新

--- WeBGame/wwwroot/js/store.js
+++ WeBGame/wwwroot/js/store.js
@@ -1,3 +1,18 @@
+var focnow = 0;
 function 精选滚动() {
+    //let foc = document.body.getElementsByClassName("focus");
+    var nfoc = document.body.getElementsByClassName("store_main_capsule");
+    var ca = document.body.getElementsByTagName("a class");
+    var fcount = nfoc.length;
+    document.body.getElementsByClassName("test")[0].innerHTML += nfoc.length;
+    setTimeout(function () {
+        var foc = document.body.getElementsByClassName("focus");
+        //let nfoc = document.body.getElementsByClassName("store_main_capsule broadcast_capsule app_impression_tracked");
+        foc[0].classList.remove("focus");
+        nfoc[focnow].classList.add("focus");
+        focnow++;
+        focnow %= fcount;
+    }, 1500);
 }
+精选滚动();
 //# sourceMappingURL=store.js.map
--- WeBGame/wwwroot/js/store.ts
+++ WeBGame/wwwroot/js/store.ts
@@ -1,2 +1,18 @@
-﻿function 精选滚动() {
+﻿let focnow = 0;
+function 精选滚动() {
+    //let foc = document.body.getElementsByClassName("focus");
+    let nfoc = document.body.getElementsByClassName("store_main_capsule");
+    let ca = document.body.getElementsByTagName("a class");
+    let fcount = nfoc.length;
+    document.body.getElementsByClassName("test")[0].innerHTML += nfoc.length;
+    setTimeout(() => {
+        let foc = document.body.getElementsByClassName("focus");
+        //let nfoc = document.body.getElementsByClassName("store_main_capsule broadcast_capsule app_impression_tracked");
+        foc[0].classList.remove("focus");
+        nfoc[focnow].classList.add("focus");
+        focnow++;
+        focnow %= fcount;
+    }, 1500);
 }
+
+精选滚动();
