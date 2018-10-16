4bedf7bdade37931aa00ca30ea4b5b7e2690f454
Egan <1154610080@qq.com>
2018-10-16 22:18:22 +0800

    修复DbSet和数据表映射失败的bug

--- WeBGame/WbDbContext.cs
+++ WeBGame/WbDbContext.cs
@@ -13,8 +13,8 @@ namespace WeBGame
 
         }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Game> Games { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Game> Game { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
