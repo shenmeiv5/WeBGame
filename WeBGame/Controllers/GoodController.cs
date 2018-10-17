ef8595b5a53e82229aa0f037b9cefc4e512525eb
shenmeiv5 <982603131@qq.com>
2018-10-17 19:22:23 +0800

    完成了商品页面-Good页面。

---
+++ WeBGame/Controllers/GoodController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeBGame.Controllers
+{
+    public class GoodController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Good");
+        }
+    }
+}
